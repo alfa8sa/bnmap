6e2be7017af0a97183ac7bc14028b0f07c765625
alfa8sa <alfagemesergio@gmail.com>
2022-04-05 17:24:24 +0200

    Update bnmap.sh

--- bnmap.sh
+++ bnmap.sh
@@ -9,22 +9,25 @@ function ctrl_c(){
 }
 
 function helpPanel(){
-	echo -e "\n[!] Use: ./bnmap.sh"
+	echo -e "\n[!] Usage: ./bnmap.sh"
 	for i in $(seq 1 85); do echo -ne "-"; done
-	echo -e "\n\t[-i] Scan interface. Default subnet mask: 255.255.255.0 (Example: -i eth0)"
+	echo -e "\n\t[-n] Scan network. (Example: -n 192.168.1.0/24)"
+	echo -e "\t[-i] Scan interface. Default subnet mask: 255.255.255.0 (Example: -i eth0)"
 	echo -e "\t[-s] Scan interface, but with subnet mask: 255.255.0.0 (Example: -s eth0)"
 	echo -e "\t[-p] Scan open ports of a host. [Firts 10000 ports] (Example: -p 192.168.1.1)"
-	echo -e "\t[-h] Show this help menu."
+	echo -e "\t[-h] Show this help menu.\n"
 	exit 1
 }
 
 function portScan(){
-	if [[ $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+	octet="(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])"
+	ip4="^$octet\\.$octet\\.$octet\\.$octet$"
+	if [[ $1 =~ $ip4 ]]; then
 		echo -e "\n[*] Scanning open ports for $1"
 		for port in $(seq 1 10000); do
 			timeout 1 bash -c "</dev/tcp/$1/$port" &>/dev/null && echo -e "\t[!] Port open: $1:$port" &
 		done; wait
-	else
+	elif [[ $1 == "" ]]; then
 		hosts=$(cat hosts | sort -t . -k 3,3n -k 4,4n)
 		for host in $hosts; do
 			echo -e "\t[*] Host $host ACTIVE"
@@ -33,26 +36,26 @@ function portScan(){
 			done; wait
 		done
 		rm -rf hosts
+	else
+		echo -e "\n[!] Invalid IPv4 address\n"
 	fi
 }
 
 function bnmap(){
-	network=$(ip -4 -o a | cut -d ' ' -f 2,7 | cut -d '/' -f 1 | grep $1 | awk '{print $2}' | sed 's/\.[0-9]*$//')
-	echo -e "\n[*] Scanning network: $network.0/24\n"
+	echo -e "\n[*] Scanning network: $1.0/24\n"
 	for i in $(seq 1 254); do
-		timeout 2 bash -c "ping -c 1 $network.$i" &>/dev/null && echo $network.$i >> hosts &
+		timeout 2 bash -c "ping -c 1 $1.$i" &>/dev/null && echo $network.$i >> hosts &
 	done; wait
 	portScan
 }
 
 function bnmap_16(){
-	network=$(ip -4 -o a | cut -d ' ' -f 2,7 | cut -d '/' -f 1 | grep $1 | awk '{print $2}' | sed 's/\.[0-9]*\.[0-9]*$//')
-	echo -e "\n[*] Scanning network: $network.0.0/16\n"
+	echo -e "\n[*] Scanning network: $1.0.0/16\n"
 	for j in $(seq 1 254); do
 		touch hosts
-		echo -e "  [*] Scanning network: $network.$j.0/24"
+		echo -e "  [*] Scanning network: $1.$j.0/24"
 		for i in $(seq 1 254); do
-			timeout 2 bash -c "ping -c 1 $network.$j.$i" &>/dev/null && echo $network.$j.$i >> hosts &
+			timeout 2 bash -c "ping -c 1 $1.$j.$i" &>/dev/null && echo $network.$j.$i >> hosts &
 		done; wait
 		portScan
 	done; wait
@@ -62,9 +65,11 @@ function check_interface(){
 	interfaces=$(/sbin/ip -4 -o a | awk '{print $2}')
 	if [[ "$interfaces" == *"$1"* ]]; then
 		if [ $2 == "i" ]; then
-			bnmap $1
+			network=$(ip -4 -o a | cut -d ' ' -f 2,7 | cut -d '/' -f 1 | grep $1 | awk '{print $2}' | sed 's/\.[0-9]*$//')
+			bnmap $network
 		elif [ $2 == "s" ]; then
-			bnmap_16 $1
+			network=$(ip -4 -o a | cut -d ' ' -f 2,7 | cut -d '/' -f 1 | grep $1 | awk '{print $2}' | sed 's/\.[0-9]*\.[0-9]*$//')
+			bnmap_16 $network
 		fi
 	else
 		interfaces=$(/sbin/ip -4 -o a | awk '{print $2}')
@@ -75,8 +80,26 @@ function check_interface(){
 	fi
 }
 
-parameter_counter=0; while getopts "i:s:p:h" arg; do
+function check_network(){
+	octet="(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])"
+	net24="^$octet\\.$octet\\.$octet\\.$octet\\/24$"
+	net16="^$octet\\.$octet\\.$octet\\.$octet\\/16$"
+	if [[ $1 =~ $net24 ]]; then
+		network=$(echo $1 | sed 's/\.[0-9]*\/24$//')
+		bnmap $network
+	elif [[ $1 =~ $net16 ]]; then
+		network=$(echo $1 | sed 's/\.[0-9]*\.[0-9]*\/16$//')
+		bnmap_16 $network
+	else
+		echo -e "\n[!] Invalid network. Valid formats:\n"
+		echo -e "\t[*] 192.168.1.0/24"
+		echo -e "\t[*] 192.168.1.0/16\n"
+	fi
+}
+
+parameter_counter=0; while getopts "n:i:s:p:h" arg; do
 	case $arg in
+		n) check_network $OPTARG;parameter_counter=1;;
 		i) check_interface $OPTARG "i";parameter_counter=1;;
 		s) check_interface $OPTARG "s";parameter_counter=1;;
 		p) portScan $OPTARG;parameter_counter=1;;
